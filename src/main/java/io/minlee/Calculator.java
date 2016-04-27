526a95c850e2569e77f6331585d61bd078d6f7bb
Min Lee <minlee@Zipcoder04s-MacBook-Pro.local>
2016-04-27 18:13:58 -0400

    Added Subtraction Multiplication Division

--- src/main/java/io/minlee/Calculator.java
+++ src/main/java/io/minlee/Calculator.java
@@ -8,4 +8,13 @@ public class Calculator {
     public  int sum(int value1, int value2){
         return value1 + value2;
     }
+    public  int subtract(int value1, int value2){
+        return value1 - value2;
+    }
+    public  int multiply(int value1, int value2){
+        return value1 * value2;
+    }
+    public  int divide(int value1, int value2){
+        return value1 / value2;
+    }
 }
--- src/test/java/io/minlee/CalculatorSpec.java
+++ src/test/java/io/minlee/CalculatorSpec.java
@@ -15,4 +15,25 @@ public class CalculatorSpec {
         int actualSum = calculator.sum(2,2);
         assertEquals("The expected value should be "+expectedSum,expectedSum,actualSum);
     }
+    @Test
+    public void subtractTest(){
+        Calculator calculator = new Calculator();
+        int expectedSubtraction = 1;
+        int actualSubtraction = calculator.subtract(3,2);
+        assertEquals("The expected value should be "+expectedSubtraction,expectedSubtraction,actualSubtraction);
+    }
+    @Test
+    public void multiplyTest(){
+        Calculator calculator = new Calculator();
+        int expectedMultiplication = 4;
+        int actualMultiplication = calculator.multiply(2,2);
+        assertEquals("The expected value should be "+expectedMultiplication,expectedMultiplication,actualMultiplication);
+    }
+    @Test
+    public void divideTest(){
+        Calculator calculator = new Calculator();
+        int expectedDivision = 1;
+        int actualDivision = calculator.divide(2,2);
+        assertEquals("The expected value should be "+expectedDivision,expectedDivision,actualDivision);
+    }
 }
